b8e8c2953732090105b115c99564254b32605f7f
alcherniaev <playtowin19@gmail.com>
2021-01-01 23:55:55 +0300

    sys argv two ways to solve

--- Problems/Types/main.py
+++ Problems/Types/main.py
@@ -1,10 +1,11 @@
 import sys
 args = sys.argv
-
-# further code of the script "process_four_numbers.py"
+'''# further code of the script "process_four_numbers.py"
 if len(args) == 5:
-    for i in range(1, 4):
+    for i in range(1, 5):
         args[i] = int(args[i])
-    print(args[1:4])
+    print(args[1:5])
 else:
-    print("The script should be called with four arguments")
+    print("The script should be called with four arguments")'''
+my_list = [int(arg) for arg in args[1:5]]
+print(my_list)
