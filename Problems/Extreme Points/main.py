e205056684916db3510efded6e96c7d3159735e2
alcherniaev <playtowin19@gmail.com>
2021-01-08 11:49:05 +0300

    dictionary min max for key values and get

---
+++ Problems/Extreme Points/main.py
@@ -0,0 +1,8 @@
+# The following line creates a dictionary from the input. Do not modify it, please
+#test_dict = json.loads(input())
+test_dict = {"a": 43, "b": 1233, "c": 8}
+# Work with the 'test_dict'
+
+min_ = min(test_dict, key=test_dict.get)
+max_ = max(test_dict, key=test_dict.get)
+print("min: " + str(min_) + "\n" + "max: " + str(max_))
