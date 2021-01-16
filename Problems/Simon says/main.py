2576ffe575dd7ab21e9f7bc6f6911f0cc02963a3
alcherniaev <playtowin19@gmail.com>
2021-01-16 14:04:52 +0300

    is it copy.deepcopy()?

--- Problems/Simon says/main.py
+++ Problems/Simon says/main.py
@@ -1,7 +1,13 @@
-def what_to_do(instructions):
+'''def what_to_do(instructions):
     say = "Simon says"
     if instructions.startswith(say) or instructions.endswith(say):
         do = instructions.replace(say, '').strip()
         return f"I {do}"
     else:
-        return "I won't do it!"
+        return "I won't do it!"'''
+
+empty_l = []
+empty_l.append(empty_l)
+
+print(empty_l)    # [[...]]
+print(empty_l.copy().copy)
