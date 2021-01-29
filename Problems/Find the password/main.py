adcd48ebd86ccc9454fde4a5c817ed7215066bab
alcherniaev <playtowin19@gmail.com>
2021-01-29 14:24:01 +0300

    add requests encoding header text get etc

--- Problems/Find the password/main.py
+++ Problems/Find the password/main.py
@@ -1,9 +1,22 @@
 from lxml import etree
-
+import requests
 
 def find_password(xml_string):
     root = etree.fromstring(xml_string)
     for i in root:
         return i.get('password')
 
+
+
+# testing requests
+r = requests.get('https://hyperskill.org/learn/step/8603')
+print(r)
+
+if r:
+    print('success')
+else:
+    print('fail')
+print(r.encoding)
+print(r.headers)
+print(r.text)
 
